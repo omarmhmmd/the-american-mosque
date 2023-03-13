8fc8960d3bd338690b82a14c2d243819a00ad48e
omarmhmmd <omar.mhmmd@gmail.com>
2023-03-12 20:54:22 -0700

    single index

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,261 @@
+import { Fragment } from "react";
 import Head from "next/head";
+import { getDatabase, getPage, getBlocks } from "../lib/notion";
 import Link from "next/link";
-import { getDatabase } from "../lib/notion";
-import Post from "./Post.js";
-import styles from "./index.module.css";
+import styles from "./post.module.css";
 
-export const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID;
+export const Text = ({ text }) => {
+  if (!text) {
+    return null;
+  }
+  return text.map((value) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = value;
+    return (
+      <span
+        className={[
+          bold ? styles.bold : "",
+          code ? styles.code : "",
+          italic ? styles.italic : "",
+          strikethrough ? styles.strikethrough : "",
+          underline ? styles.underline : "",
+        ].join(" ")}
+        style={color !== "default" ? { color } : {}}
+        key={text.content}
+      >
+        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+      </span>
+    );
+  });
+};
 
-export default function Home({ posts }) {
+const renderNestedList = (block) => {
+  const { type } = block;
+  const value = block[type];
+  if (!value) return null;
+
+  const isNumberedList = value.children[0].type === "numbered_list_item";
+
+  if (isNumberedList) {
+    return <ol>{value.children.map((block) => renderBlock(block))}</ol>;
+  }
+  return <ul>{value.children.map((block) => renderBlock(block))}</ul>;
+};
+
+const renderBlock = (block) => {
+  const { type, id } = block;
+  const value = block[type];
+  switch (type) {
+    case "paragraph":
+      return (
+        <p>
+          <Text text={value.rich_text} />
+        </p>
+      );
+    case "callout":
+      return (
+        <p className={styles.callOut}>
+          <Text text={value.rich_text} />
+        </p>
+      );
+    case "heading_1":
+      return (
+        <h1>
+          <Text text={value.rich_text} />
+        </h1>
+      );
+    case "heading_2":
+      return (
+        <h2>
+          <Text text={value.rich_text} />
+        </h2>
+      );
+    case "heading_3":
+      return (
+        <h3>
+          <Text text={value.rich_text} />
+        </h3>
+      );
+    case "bulleted_list": {
+      return <ul>{value.children.map((child) => renderBlock(child))}</ul>;
+    }
+    case "numbered_list": {
+      return <ol>{value.children.map((child) => renderBlock(child))}</ol>;
+    }
+    case "bulleted_list_item":
+    case "numbered_list_item":
+      return (
+        <li key={block.id}>
+          <Text text={value.rich_text} />
+          {!!value.children && renderNestedList(block)}
+        </li>
+      );
+    case "to_do":
+      return (
+        <div>
+          <label htmlFor={id}>
+            <input type="checkbox" id={id} defaultChecked={value.checked} />{" "}
+            <Text text={value.rich_text} />
+          </label>
+        </div>
+      );
+    case "toggle":
+      return (
+        <details>
+          <summary>
+            <Text text={value.rich_text} />
+          </summary>
+          {block.children?.map((child) => (
+            <Fragment key={child.id}>{renderBlock(child)}</Fragment>
+          ))}
+        </details>
+      );
+    case "child_page":
+      return (
+        <div className={styles.childPage}>
+          <strong>{value.title}</strong>
+          {block.children.map((child) => renderBlock(child))}
+        </div>
+      );
+    case "image":
+      const src =
+        value.type === "external" ? value.external.url : value.file.url;
+      const caption = value.caption ? value.caption[0]?.plain_text : "";
+      const link = value.caption ? value.caption[1]?.plain_text : "";
+      return (
+        <figure>
+          <img src={src} alt={caption} />
+          {caption && (
+            <figcaption>
+              {caption} <a href={value.caption[1]?.href}>{link}</a>{" "}
+            </figcaption>
+          )}
+        </figure>
+      );
+    case "divider":
+      return <hr key={id} />;
+    case "embed":
+      let iframeLink = block.embed.url;
+      iframeLink = iframeLink.replace("edit", "embed");
+      return (
+        <iframe
+          src={iframeLink}
+          title={`iframe ${block.type}`}
+          allowFullScreen
+          loading="lazy"
+        />
+      );
+    case "quote":
+      return <blockquote key={id}>{value.rich_text[0].plain_text}</blockquote>;
+    case "code":
+      return (
+        <pre className={styles.pre}>
+          <code className={styles.code_block} key={id}>
+            {value.rich_text[0].plain_text}
+          </code>
+        </pre>
+      );
+    case "file":
+      const src_file =
+        value.type === "external" ? value.external.url : value.file.url;
+      const splitSourceArray = src_file.split("/");
+      const lastElementInArray = splitSourceArray[splitSourceArray.length - 1];
+      const caption_file = value.caption ? value.caption[0]?.plain_text : "";
+      return (
+        <figure>
+          <div className={styles.file}>
+            📎{" "}
+            <Link href={src_file} passHref>
+              {lastElementInArray.split("?")[0]}
+            </Link>
+          </div>
+          {caption_file && <figcaption>{caption_file}</figcaption>}
+        </figure>
+      );
+    case "bookmark":
+      const href = value.url;
+      return (
+        <a href={href} target="_brank" className={styles.bookmark}>
+          {href}
+        </a>
+      );
+    case "table": {
+      return (
+        <table className={styles.table}>
+          <tbody>
+            {block.children?.map((child, i) => {
+              const RowElement =
+                value.has_column_header && i == 0 ? "th" : "td";
+              return (
+                <tr key={child.id}>
+                  {child.table_row?.cells?.map((cell, i) => {
+                    return (
+                      <RowElement key={`${cell.plain_text}-${i}`}>
+                        <Text text={cell} />
+                      </RowElement>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      );
+    }
+    case "column_list": {
+      return (
+        <div className={styles.row}>
+          {block.children.map((block) => renderBlock(block))}
+        </div>
+      );
+    }
+    case "column": {
+      return <div>{block.children.map((child) => renderBlock(child))}</div>;
+    }
+    default:
+      return `❌ Unsupported block (${
+        type === "unsupported" ? "unsupported by Notion API" : type
+      })`;
+  }
+};
+
+export default function Post({ database, page, blocks }) {
+  console.log(database)
+  if (!page || !blocks) {
+    return <div>hey</div>;
+  }
   return (
     <div>
       <Head>
-        <title>omars blog</title>
+        <title>{page.properties.Name.title[0].plain_text}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.container}>
-        <Post id={posts[0].id}/>
-        {/* <div className={styles.posts}>
-          {posts.map((post) => {
-            console.log(posts)
-            const date = new Date(post.last_edited_time).toLocaleString(
-              "en-US",
-              {
-                month: "short",
-                day: "2-digit",
-                year: "numeric",
-              }
-            );
-            return (
-              <div key={post.id} className={styles.post}>
-                <div className={styles.postDescription}>{date}</div>
-                <Link href={`/${post.id}`}>{post.id} →</Link>
-              </div>
-            );
-          })}
-        </div> */}
-      </main>
+
+      <article className={styles.container}>
+        <h1 className={styles.name}>
+          <Text text={page.properties.Name.title} />
+        </h1>
+        <section>
+          {blocks.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+        </section>
+      </article>
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const database = await getDatabase(databaseId);
-
+  // const database = await getDatabase('3c2755967a3d41f48add6dd8c39d36ac');
+  const page = await getPage(process.env.NEXT_PUBLIC_NOTION_PAGE_ID);
+  const blocks = await getBlocks(process.env.NEXT_PUBLIC_NOTION_PAGE_ID);
   return {
     props: {
-      posts: database,
+      // database,
+      page,
+      blocks,
     },
     revalidate: 1,
   };
