4f51da6fd02eb48213a770157c9539c638c4723d
omarmhmmd <omar.mhmmd@gmail.com>
2023-03-25 18:23:51 -0700

    colors

--- components/panel/Panel.js
+++ components/panel/Panel.js
@@ -15,9 +15,12 @@ export default function Panel({ page, blocks }) {
               <h1 className={styles.sub}>
                 <Text text={page.properties.Subtitle.rich_text} />
               </h1>
+              {/* <h3 className={styles.author}>
+                Omar Mohammad
+              </h3> */}
             </header>
-            <div className={styles.imgGrid}>
-              {page.properties["Files & media"].files.map((img) => {
+            <div className={styles.imgSingle}>
+              {page.properties.Pic.files.map((img) => {
                 return (
                   <Image src={img.external.url}
                     width={600}
@@ -27,21 +30,31 @@ export default function Panel({ page, blocks }) {
                 // return <img src={img.file.url} />;
               })}
             </div>
-            <div className={styles.info}>
-              <div className={styles.text}>
-                {/* <h1 className={styles.infoHeader}>Info</h1> */}
-                <p style={{ paddingBottom: "8px" }}>
-                  <Text text={page.properties.Author.rich_text} />
-                </p>
-                <p style={{ paddingBottom: "0px" }}>
-                  <Text text={page.properties.Info.rich_text} />
-                </p>
-                {/* <h1 className={styles.infoHeader}>Colophon</h1>
+            {/* <div className={styles.imgGrid}>
+              {page.properties.Pic.files.map((img) => {
+                return (
+                  <Image src={img.external.url}
+                    width={600}
+                    height={800}
+                  />
+                )
+                // return <img src={img.file.url} />;
+              })}
+            </div> */}
+            <div className={styles.text}>
+              {/* <h1 className={styles.infoHeader}>Info</h1> */}
+              <p style={{ paddingBottom: "8px" }}>
+                <Text text={page.properties.Author.rich_text} />
+              </p>
+              <p style={{ paddingBottom: "0px" }}>
+                <Text text={page.properties.Info.rich_text} />
+              </p>
+              {/* <h1 className={styles.infoHeader}>Colophon</h1>
                 <p>
                   <Text text={page.properties.Colophon.rich_text} />
                 </p> */}
-              </div>
             </div>
+
           </div>
         </div>
       </section>
