97f13c0d7c643fd4489541e20266eedf567b3eca
omarmhmmd <omar.mhmmd@gmail.com>
2023-03-25 19:03:00 -0700

    color and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
 import "../styles/globals.css";
+import localFont from '@next/font/local'
+
+const graphik = localFont({
+  src: [
+    {
+      path: './fonts/Graphik-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Graphik-RegularItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Graphik-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Graphik-SemiBold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Graphik-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={graphik.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp;
